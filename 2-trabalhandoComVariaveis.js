230eda7fbf7c1312d32f2afd5d4bb87b920758b5
Vinicius Dorlitz <viniciusdorlitz@gmail.com>
2021-11-04 21:08:38 -0300

    adição de 2 arquivos (3-operacoesAritméticas e 4-conversaoDeTipos)

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//JS é case sensitive
+//Console.log("Trabalhando com variáveis"); Da erro
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+const nome = "Ricardo";
+let ano = 2021;
+
+// não fazer
+// mes = "Janeiro" 
