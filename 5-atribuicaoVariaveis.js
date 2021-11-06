ab5b7e498ac0a32911d6a5c2bed299c28e11618d
Vinicius Dorlitz <viniciusdorlitz@gmail.com>
2021-11-05 21:04:28 -0300

    Adicionado o arquivo (5-atribuiaoVariaveis.js)

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,17 @@
+
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 27;
+const primeiroNome = "Vinicius";
+const sobrenome = "Dorlitz";
+
+// console.log(nome + sobrenome); Ficou sem espaço, a duas formas de fazer isso:
+// console.log(nome + " " + sobrenome);
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+
